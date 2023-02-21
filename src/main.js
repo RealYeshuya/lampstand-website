ee510f7efc25f3e4f40b874557a40950d534d952
RealYeshuya <jpang6191@gmail.com>
2023-02-21 13:15:54 +0800

    build: Add graphql and tailwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/styles.css";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
-createApp(App).use(router).mount("#app");
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:1337/graphql",
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
+
+app.use(router).mount("#app");
